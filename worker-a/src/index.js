be827b714718fb90fd94441bc73f3a9eb7d04f9b
calltella <yuichi.asa@gmail.com>
2026-02-07 12:48:44 +0000

    vscode add

--- worker-a/src/index.js
+++ worker-a/src/index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     // RPC 呼び出しで add を呼ぶ
-    const result = await env.WORKER_B.add(3, 5);
+    const result = await env.WORKER_B.add(3, 6);
     return new Response(`Result from WorkerB.add: ${result}`);
   },
 };
